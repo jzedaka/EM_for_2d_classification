c0c149e10f4387125e43bd36ddd1c2472d1b305e
jzedaka <35489421+jzedaka@users.noreply.github.com>
2025-03-28 18:05:51 +0300

    Update em.py

--- em.py
+++ em.py
@@ -6,7 +6,9 @@ import concurrent.futures
 
 class EM():
 
-    def __init__(self, img_shape, rotation_res=5, scale_res=0.1, trans_res=1, max_iter=10):
+    def __init__(self, img_shape, rotation_res=5, scale_res=0.1,
+                 trans_res=1, max_iter=10, verbose=False):
+        self.verbose = verbose
         self.J = img_shape[0] * img_shape[1]
         self.translations_sigma = 0.05 * img_shape[0]
         self.rotation_res = rotation_res
@@ -16,7 +18,7 @@ class EM():
         scales = np.linspace(0.5, 1.5, n_scales)
         n_trans = int(2 * 0.05 * img_shape[0] // trans_res)
         translations = np.linspace(-int(0.05 * img_shape[0]), int(0.05 * img_shape[0]), n_trans).astype(np.int32)
-        # translations = np.array([1])
+        # translations = np.array([0])
         self.phis = np.zeros(
             [rots.size*scales.size*translations.size*translations.size, 2, 3])
         self.lpriors = np.zeros(
@@ -35,17 +37,22 @@ class EM():
                         self.phis[c][1, 2] += t_y
                         c += 1
 
-        print(f'model is ready. table size = {self.phis.shape}')
+        if self.verbose:
+            print(f'model is ready. table size = {self.phis.shape}')
 
         self.img_shape = img_shape
         self.max_iter = max_iter
 
     def lprior(self, rot, scale, t_x, t_y):
-        lp =  -1 * ((t_x ** 2) + (t_y ** 2)) / (2 * self.translations_sigma ** 2) + np.log(scale) + 4*np.log(1 - (scale/5)) + np.log(6/5) 
+        lp =  -1 * ((t_x ** 2) + (t_y ** 2)) / (2 * self.translations_sigma ** 2) + np.log(scale) + 4*np.log(1 - (scale/5))
         return lp
 
     def ll(self, phiX, A, sigma):
-        return -1 * np.sum((A-phiX)**2) / (2*(sigma**2))
+        """
+        Calculate the log-liklihood of the transformed image phiX.
+        the constant terms are neglected.
+        """
+        return -1 * np.sum(np.square(A-phiX)) / (2*(sigma**2))
 
     def single_img(self, i):
         new_A = np.zeros_like(self.A)
@@ -54,18 +61,19 @@ class EM():
 
         def single_phi(j):
             self.phiXs[j] = cv2.warpAffine(self.images[i], self.phis[j], self.img_shape[::-1],
-                                           flags=cv2.INTER_NEAREST,
-                                           borderMode=cv2.BORDER_REPLICATE).astype(np.float16)
+                                           flags=cv2.INTER_NEAREST).astype(np.float16)
             self.lls[i, j] = self.ll(phiX=self.phiXs[j], A=self.A, sigma=self.sigma) + self.lpriors[j]
 
+        # multi-threading to reduce run-time
         with concurrent.futures.ThreadPoolExecutor() as executor:
                 list(executor.map(single_phi, list(range(self.phis.shape[0]))))
 
         max_ll = np.max(self.lls[i])
         log_denom = np.log(np.sum(np.exp(self.lls[i] - max_ll))) + max_ll
-        denom = np.exp(log_denom)
         self.ws[i] = np.exp(self.lls[i] - log_denom)
-        for j in range(self.phis.shape[0]):
+        non_zeros = np.argwhere(self.ws[i] > 0)[:, 0]
+        # print(non_zeros.size)
+        for j in non_zeros:
             new_A += (1 / self.images.shape[0]) * self.ws[i, j] * self.phiXs[j]
             sigma_sqr += (1 / (self.images.shape[0]*self.J)) * \
                 self.ws[i, j] * np.sum((self.A-self.phiXs[j])**2)
@@ -76,32 +84,33 @@ class EM():
         self.images = images
 
         # initialize parameters
-        self.sigma = 20
+        self.sigma = np.mean(np.abs(images))
         self.A = images[0]
         # self.A = np.mean(images, axis=0)
 
         self.ws = np.zeros((images.shape[0], self.phis.shape[0]))
         self.lls = np.zeros((images.shape[0], self.phis.shape[0]))
         for iteration in range(self.max_iter):
-            print(f'Iter {iteration}')
+            if self.verbose:
+                print(f'Iter {iteration}')
             
             new_A = np.zeros_like(self.A)
             sigma_sqr = 0
             for i, X_i in enumerate(self.images):
-                res = self.single_img(i)
-                new_A += res[0]
-                sigma_sqr += res[1] 
+                dA, dS = self.single_img(i)
+                new_A += dA
+                sigma_sqr += dS
 
             self.sigma = np.sqrt(sigma_sqr)
-
-            print("max diff")
-            max_change = np.max(np.abs(self.A-new_A))
-            print(max_change)
-            if max_change < 20:
-                return new_A
-
+            realtive_change = np.mean(np.abs((self.A-new_A))) / (np.mean(np.abs(self.A)) + 1e-4)
+            if self.verbose:
+                print(f"Realtive_change = {realtive_change}")
             self.A = new_A
-            print(f'Sigma = {self.sigma* 1}')
+            if realtive_change < 0.01:
+                return self.A
+
+            if self.verbose:
+                print(f'Sigma = {self.sigma}')
             # plt.imshow(self.A, cmap='gray')
             # plt.axis('off')
             # plt.show()
