65a10dc4d03ebf97c7df6bb97d10ef6c6c2382e7
jzedaka <35489421+jzedaka@users.noreply.github.com>
2025-03-28 19:22:38 +0300

    Update results_section.py

--- results_section.py
+++ results_section.py
@@ -9,7 +9,10 @@ from main import get_data
 
 
 def calc_mse(img, rec_img):
-    return np.mean((img-rec_img)**2)
+    e = []
+    for s in np.linspace(0.5, 1.5, 11):
+        e.append(np.mean((img-s*rec_img)**2))
+    return np.min(e)
 
 def exp_1(M=10):
     Ns = np.array([5, 10, 30])
@@ -32,6 +35,7 @@ def exp_1(M=10):
                 print(f'n = {n}, sigma = {sigma}, rep = {k}, err = {errors[i, j, k]}')
 
     plt.figure()
+
     for i, n in enumerate(Ns):
         means = np.mean(errors[i, :], axis=1)
         plt.plot(sigmas, means, '--o', label=f"n = {n}")
@@ -57,7 +61,40 @@ def exp_1(M=10):
     plt.ylabel("MSE")
     plt.title("Error vs n")
 
+def exp_2(M=20):
+    sigmas = np.array([0.001, 0.01, 0.1, 1, 2])
+    recon_snr = np.zeros((sigmas.size, M))
+    data_snr = np.zeros((sigmas.size, M))
+    n = 5
+    for j, sigma in enumerate(sigmas):
+        for k in range(M):
+            images, img = get_data(N=n, sigma=sigma, translations=False)
+            em = EM(img_shape=images[0].shape,
+                    rotation_res=5,
+                    scale_res=0.2,
+                    trans_res=3,
+                    max_iter=10,
+                    verbose=False)
+
+            rec_img = em.recover_img(images)
+            recon_snr[j, k] = calc_mse(img=img, rec_img=rec_img)
+            data_snr[j, k] = np.mean((images-img)**2)
+            print(f'n = {n}, sigma = {sigma}, rep = {k}, snr = {recon_snr[j, k]}')
+
+    data_snr = np.mean(data_snr, axis=-1)
+    plt.figure()
+
+    means = np.mean(recon_snr, axis=1)
+    plt.plot(data_snr, means, '--o')
+    stds = np.std(recon_snr, axis=1)
+    plt.fill_between(data_snr, means-stds, means+stds, alpha=0.3)
+    
+    plt.grid()
+    plt.xlabel("Data SNR")
+    plt.ylabel("Reconstruction SNR")
+    plt.xscale("log")
 
 if __name__ == "__main__":
     exp_1()
+    exp_2()
     plt.show()
