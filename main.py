57b0feb49b43fd77e0d6e79c990555170d7a53c1
jzedaka <35489421+jzedaka@users.noreply.github.com>
2025-03-28 18:06:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-import numpy as np
-
+import argparse
 import cv2
+import mrcfile
+import numpy as np
 import matplotlib.pyplot as plt
 from em import EM
 from time import time
 
 
-def get_data(N=100, sigma=1):
-    # 1. Load the sample face (RGB) from SciPy
-    # image_rgb = face()
-    image_path = r"C:\Users\jonathanz\Desktop\pin.png"
-    image_rgb = cv2.imread(image_path)
-    # 2. Convert to 2D grayscale
-    #    The result is a 2D array (height x width)
-    img = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2GRAY)
+def get_data(N=100, sigma=1, save=False, translations=True):
+
+    # image_path = r"C:\Users\jonathanz\Desktop\pin.png"
+    # image_rgb = cv2.imread(image_path)
+    S = 64
+    cx, cy = S // 2, S // 2
+    img = np.zeros((S, S))
+    radius = 16
+    # Fill the array
+    for x in range(S):
+        for y in range(S):
+            # Compute squared distance from the center
+            if (x - cx)**2 + (y - cy)**2 <= radius**2:
+                img[x, y] = 1
+    
     # plt.imshow(img, cmap='gray')
     # plt.axis('off')
     # plt.show()
     # Get image dimensions
     h, w = img.shape
 
-    images = np.zeros((N, h, w))
+    images = np.zeros((N, h, w), dtype=np.float32)
     for i in range(N):
         angle = np.random.uniform(0, 360)
         scale = np.random.uniform(0.5, 1.5)
@@ -31,12 +39,13 @@ def get_data(N=100, sigma=1):
             center=(w/2, h/2), angle=angle, scale=scale)
 
         # The translation component is stored in M[0,2] and M[1,2]
-        M[0, 2] += tx
-        M[1, 2] += ty
+        if translations:
+            M[0, 2] += tx
+            M[1, 2] += ty
 
         # Apply the affine transformation
         transformed_img = cv2.warpAffine(
-            img, M, (w, h), flags=cv2.INTER_NEAREST).astype(np.float32)
+            img, M, (w, h), flags=cv2.INTER_LINEAR).astype(np.float32)
 
         # add noise
         transformed_img += np.random.randn(*transformed_img.shape) * sigma
@@ -47,23 +56,40 @@ def get_data(N=100, sigma=1):
         # plt.axis('off')
         # plt.show()
 
-    return images
+    if save:
+        with mrcfile.new(r'C:\Users\jonathanz\Desktop\input_images.mrcs', overwrite=True) as mrc:
+            mrc.set_data(images)
+
+    return images, img
 
 
 if __name__ == "__main__":
 
-    images = get_data(N=100, sigma=50)
+    # Parse the command line inputs
+    parser = argparse.ArgumentParser(description='EM arguments parser.')
+    parser.add_argument('--mrc_stack_path', type=str, required=False, help='--mrc_stack_path <path to images>')
+    sys_args, unknown = parser.parse_known_args()
+
+    if sys_args.mrc_stack_path is not None:
+        print("Reading images from file ...")
+        with mrcfile.open(sys_args.mrc_stack_path, permissive=True) as mrc:
+            images = mrc.data
+    else:
+        images, img = get_data(N=100, sigma=0.05)
+
     em = EM(img_shape=images[0].shape,
-            rotation_res=10,
-            scale_res=0.1,
-            trans_res=6,
-            max_iter=5)
+            rotation_res=5,
+            scale_res=0.2,
+            trans_res=3,
+            max_iter=10,
+            verbose=True)
 
     t0 = time()
-    img = em.recover_img(images)
+    rec_img = em.recover_img(images)
 
     t1 = time()
     print(f"runtime = {(t1 - t0) / 60} min")
-    plt.imshow(img, cmap='gray')
+    plt.imshow(rec_img, cmap='gray')
     plt.axis('off')
     plt.show()
+    print('Done')
